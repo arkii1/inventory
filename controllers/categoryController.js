0022a96c108bd9ceb11e5ab9864f03e925890cd6
arkii1 <tommy.mapp@hotmail.com>
2022-05-25 08:54:49 +0100

    Add controllers

---
+++ controllers/categoryController.js
@@ -0,0 +1,36 @@
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+// Display list of all items
+exports.category_item_list = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Category Item List")
+}
+
+// Display item create form get
+exports.category_create_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Category Create GET")
+}
+
+// Handle item create form post
+exports.category_create_post = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Category Create POST")
+}
+
+// Display item delete form get
+exports.category_delete_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Category Delete GET")
+}
+
+// Handle item delete form post
+exports.category_delete_post = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Category Delete POST")
+}
+
+// Display item update form get
+exports.category_update_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Category Update GET")
+}
+
+// Handle item update form post
+exports.category_update_post = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Category Update POST")
+}
---
+++ controllers/itemController.js
@@ -0,0 +1,45 @@
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+exports.index = function (req, res) {
+  res.send("NOT IMPLEMENTED: Index")
+}
+
+// Display list of all items
+exports.item_list = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item List")
+}
+
+// Display item detail page
+exports.item_detail = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item Detail")
+}
+
+// Display item create form get
+exports.item_create_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item Create GET")
+}
+
+// Handle item create form post
+exports.item_create_post = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item Create POST")
+}
+
+// Display item delete form get
+exports.item_delete_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item Delete GET")
+}
+
+// Handle item delete form post
+exports.item_delete_post = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item Delete POST")
+}
+
+// Display item update form get
+exports.item_update_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item Update GET")
+}
+
+// Handle item update form post
+exports.item_update_post = function (req, res, next) {
+  res.send("NOT IMPLEMENTED: Item Update POST")
+}
