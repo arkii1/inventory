a75b7991eca9935a3d1ef31733610f0d418bff70
arkii1 <tommy.mapp@hotmail.com>
2022-06-02 10:42:41 +0100

    Remove es-lint no console.error warning comment

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const mongoDB =
   "mongodb+srv://dev-account:123@cluster0.byqj9.mongodb.net/inventory?retryWrites=true&w=majority"
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-// eslint-disable-next-line no-console
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
 
 // Import routes
