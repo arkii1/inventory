0870cbce2308eb49b70cf0092c1866ee5e2c362d
arkii1 <tommy.mapp@hotmail.com>
2022-06-02 10:39:56 +0100

    Update url virtual

--- models/item.js
+++ models/item.js
@@ -13,7 +13,7 @@ const ItemSchema = new Schema({
 })
 
 ItemSchema.virtual("url").get(function () {
-  return `/catalog/${this._id}`
+  return `/catalog/item/${this._id}`
 })
 
 module.exports = mongoose.model("Item", ItemSchema)
