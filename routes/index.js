aade55cbcf20f25050c8e8c643b35fb343cc4c4e
arkii1 <tommy.mapp@hotmail.com>
2022-06-15 11:32:12 +0100

    Move all routes to index router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,42 @@
+/* eslint-disable no-restricted-syntax */
 const express = require("express")
 
 const router = express.Router()
+const itemController = require("../controllers/itemController")
+const categoryController = require("../controllers/categoryController")
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" })
-})
+router.get("/", itemController.index)
+
+router.get("/items", itemController.all_items)
+
+router.get("/item/create", itemController.item_create_get)
+
+router.post("/item/create", itemController.item_create_post)
+
+router.get("/item/:id/delete", itemController.item_delete_get)
+
+router.post("/item/:id/delete", itemController.item_delete_post)
+
+router.get("/item/:id/update", itemController.item_update_get)
+
+router.post("/item/:id/update", itemController.item_update_post)
+
+router.get("/item/:id", itemController.item_detail)
+
+router.get("/categories", categoryController.category_list)
+
+router.get("/category/create", categoryController.category_create_get)
+
+router.post("/category/create", categoryController.category_create_post)
+
+router.get("/category/:id/delete", categoryController.category_delete_get)
+
+router.post("/category/:id/delete", categoryController.category_delete_post)
+
+router.get("/category/:id/update", categoryController.category_update_get)
+
+router.post("/category/:id/update", categoryController.category_update_post)
+
+router.get("/category/:id", categoryController.category_item_list)
 
 module.exports = router
